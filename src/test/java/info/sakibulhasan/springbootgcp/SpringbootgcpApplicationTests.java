119c30523523d4c0e4ef7c21818f275a353b2d1d
Md Sakibul Hasan <sakib.iut@gmail.com>
2023-07-10 05:25:32 -0400

    initial commit

---
+++ src/test/java/info/sakibulhasan/springbootgcp/SpringbootgcpApplicationTests.java
@@ -0,0 +1,13 @@
+package info.sakibulhasan.springbootgcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
